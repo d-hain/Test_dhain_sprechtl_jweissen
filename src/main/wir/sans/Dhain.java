02b034d4c07c8fa1cfeaea915601fd41c43b3910
dhain <dhain@htl-steyr.ac.at>
2022-01-20 16:12:04 +0100

    added psvm in Dhain.java

--- src/main/wir/sans/Dhain.java
+++ src/main/wir/sans/Dhain.java
@@ -3,4 +3,8 @@ package wir.sans;
 public class Dhain{
     // WAHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
     // Hallo
+    public static void main(String[] args){
+        System.out.println("MOIZEIIIIT"
+        );
+    }
 }
