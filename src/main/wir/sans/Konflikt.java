52cd5f41ff5ce7b84c3fa2965b8fd1982c865370
dhain <dhain@htl-steyr.ac.at>
2022-01-20 16:25:42 +0100

    E in Konflikt.java

--- src/main/wir/sans/Konflikt.java
+++ src/main/wir/sans/Konflikt.java
@@ -3,5 +3,8 @@ package wir.sans;
 public class Konflikt {
     public static void main(String[] args) {
         //asdiub
+        int E = 15;
+        E++;
+        System.out.println(E);
     }
 }
