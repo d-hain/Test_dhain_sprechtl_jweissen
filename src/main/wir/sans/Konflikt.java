26564db3642a9065564fe5e84f8cc930f2a967d5
s-prechtl <stefan@tague.at>
2022-01-20 16:39:37 +0100

    Revert "Konflikt aiudiabdwod"
    This reverts commit 3027293359d879f3a7f292cf4ff392ea5cb435d2.
    
    # Conflicts:
    #	src/main/wir/sans/Konflikt.java

--- src/main/wir/sans/Konflikt.java
+++ src/main/wir/sans/Konflikt.java
@@ -3,11 +3,5 @@ package wir.sans;
 public class Konflikt {
     public static void main(String[] args) {
         //asdiub
-        int E = 15;
-        E++;
-        System.out.println(E);
-        //infaßfwina
-        //schusch
-        //aiudaod wa09j0da ß0ada´0w
     }
 }
